24947ff5f849906af27cf24e9e7ee3328fdd889f
Adriam Familia <adrianfamily09@gmail.com>
2020-08-18 19:25:30 -0400

    Updates

---
+++ precache-manifest.f6e5a2b2b1cd4d0e13d6474a430b124e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693ddb6b6b9d136a7b7c1051251d3603",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "06583857dce5aca5b200",
+    "url": "/newsapp/static/css/2.5e3fcda8.chunk.css"
+  },
+  {
+    "revision": "015a2814d6f0e72e7461",
+    "url": "/newsapp/static/css/main.4e673c06.chunk.css"
+  },
+  {
+    "revision": "06583857dce5aca5b200",
+    "url": "/newsapp/static/js/2.959ad650.chunk.js"
+  },
+  {
+    "revision": "ae69a686e1f284d78b6ec2bbf4577426",
+    "url": "/newsapp/static/js/2.959ad650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015a2814d6f0e72e7461",
+    "url": "/newsapp/static/js/main.39507bfb.chunk.js"
+  },
+  {
+    "revision": "16b176d0d02db5a4badd",
+    "url": "/newsapp/static/js/runtime-main.a0fbf89f.js"
+  }
+]);
